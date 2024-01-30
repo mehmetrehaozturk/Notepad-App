01030ea98873d3482d02db8666696f59fc53ec17
Reha ÖZTÜRK <147929988+mehmetrehaozturk@users.noreply.github.com>
2024-01-31 02:04:16 +0300

    create notepad-app project

---
+++ notepad.py
@@ -0,0 +1,15 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtPrintSupport import *
+
+import os, sys
+
+from PyQt5.QtWidgets import QWidget
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = ..., flags: WindowFlags | WindowType = ...) -> None:
+        super().__init__(parent, flags)
+
+    self.editor
